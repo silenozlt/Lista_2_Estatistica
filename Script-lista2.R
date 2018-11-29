2cc6b01cf33d4d84750b4e52d05f46870e7ee3f8
Cassio Philipe Placido <silenozlt@hotmail.com>
2018-11-29 20:25:48 -0200

    Questao 1

--- Script-lista2.R
+++ Script-lista2.R
@@ -1,5 +1,14 @@
 #lista 2
 #CARREGANDO ARQUIVO CBO2002_Familia.csv
 
+#Setando diretorio de trabalho
 setwd("/Users/cassio/Dropbox/Pos Data Science/Lista_2_Estatistica")
-dd
+
+#Carregando base de dados
+destilados <- read.csv("dados_dest.txt", sep = "\t", header = TRUE,na.strings = '')
+
+View(destilados)
+
+
+#1. Construa o gra ́fico de dispersa ̃o entre o n ́ıvel de hidrocarbonetos (X) e a pureza do oxigˆenio (Y).
+#Analisando o gr ́afico, o que podemos concluir sobre a relac ̧ ̃ao entre X e Y?
