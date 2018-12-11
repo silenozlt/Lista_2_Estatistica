e683d5d849ed1b754939a4f7e079c40f15eb785c
Cassio Philipe Placido <silenozlt@hotmail.com>
2018-12-10 22:11:04 -0200

    A NOITE RENDEU

--- Script-lista2.R
+++ Script-lista2.R
@@ -12,3 +12,21 @@ View(destilados)
 
 #1. Construa o gra ́fico de dispersa ̃o entre o n ́ıvel de hidrocarbonetos (X) e a pureza do oxigˆenio (Y).
 #Analisando o gr ́afico, o que podemos concluir sobre a relac ̧ ̃ao entre X e Y?
+plot(destilados, xlab = "Nivel Hidrocarbonetos", ylab = "Pureza Oxigenio")
+
+
+
+#2. Calcule o coeficiente de correlac̃ao de Pearson entre X e Y
+# Qual a interpretac ̧ ̃ao sobre o coeficiente? Ele confirma a sua resposta do item 1?
+abline(lm(destilados$y~destilados$x), h=0,col="blue")
+
+
+
+#3. Ajuste o modelo de regressa ̃o entre X e Y. Quais os valores ajustados para os coeficientes β0 (intercepto) e β1?
+destilados_ajuste=lm(destilados$y~destilados$x)
+destilados_ajuste
+#Resp : 74.28 e 14.95
+
+#4. A relacao linear entre o nıvel de hidrocarbonetos (X) e a pureza do oxigenio (Y)  e significativa?
+
+
